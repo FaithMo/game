fdffbc3814de0bb23b6dff927d0071f8080a917f
FaithMo <faithmofokeng5@gmail.com>
2019-10-31 10:31:49 +0200

    -

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-// const cards = global.window.querySelectorAll(".tiles");
-const cards = document.querySelectorAll(".tiles");
+const cards = global.window.querySelectorAll(".tiles");
+// const cards = document.querySelectorAll(".tiles");
 let firstCard,
   secondCard,
   hasFlipped = false;
